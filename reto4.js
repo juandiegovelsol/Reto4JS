c890608d89b40d9326636bc86d6ff7207e5fb6da
Juan <juandiegovelsol@gmail.com>
2022-12-05 16:32:19 -0500

    Modifica el codigo usando optional chaining

--- reto4.js
+++ reto4.js
@@ -3,11 +3,15 @@ console.log(myFunction({ a: { b: { c: 3 } } }));
 console.log(myFunction({ b: { a: 1 } }));
 console.log(myFunction({ a: { b: 2 } }));
 function myFunction(obj) {
-  if (obj.a) {
+  //Con optional chaining se puede hacer el ejercicio de una forma mucho mas sencilla
+  const value = obj.a?.b;
+  return value;
+  //Esta es la anterior forma que se puede hacer, cumplen la misma funcion
+  /* if (obj.a) {
     if (obj.a.b) {
       return obj.a.b;
     }
     return undefined;
   }
-  return undefined;
+  return undefined; */
 }
